263373eb642bb362dc0aeb9929d0d2085f12f65b
Sungguk Lim <limasdf@gmail.com>
2014-02-16 19:32:15 +0900

    Add intiaal dart version

---
+++ beagle-term/beagle.dart
@@ -0,0 +1,19 @@
+// TODO: Add license.
+
+void main() {
+  Beagle beagle = new Beagle();
+  beagle.start();
+}
+
+class Beagle {
+  void initialize() {
+    // TODO: Impelment.
+  }
+  void start() {
+    // TODO: Open connection dialog.
+    print("Open connection dialog.");
+  }
+  Beagle() {
+    initialize();
+  }
+}
